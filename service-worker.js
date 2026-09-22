4415ae1fe5f91ca09d0289546bd3df523a142860
mjgreenie8100-collab <mjgreenie8100@gmail.com>
2026-09-22 12:51:58 +0100

    Refresh cache for workflow confirmation build

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='courierflow-driver-v6';
+const CACHE='courierflow-driver-v7';
 const FILES=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 
 self.addEventListener('install',e=>{
