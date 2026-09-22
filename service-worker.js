96c8dcda54b3a5c723b9371614129b9f54a9804b
mjgreenie8100-collab <mjgreenie8100@gmail.com>
2026-09-22 12:31:57 +0100

    Refresh cache for multi-booking test build

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='courierflow-driver-v5';
+const CACHE='courierflow-driver-v6';
 const FILES=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 
 self.addEventListener('install',e=>{
