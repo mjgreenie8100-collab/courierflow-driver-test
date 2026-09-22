0464bb18c1d5921cb1c86349056b694990cd4bf8
mjgreenie8100-collab <mjgreenie8100@gmail.com>
2026-09-22 11:27:18 +0100

    Add service-worker.js for Courier Flow Driver test

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='courierflow-driver-v4';
+const FILES=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const c=res.clone();caches.open(CACHE).then(k=>k.put(e.request,c));return res}).catch(()=>caches.match('./index.html'))))});
